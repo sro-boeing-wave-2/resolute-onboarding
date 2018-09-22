4eb6128a523f815358eb6dc6b9c6912f98bd87ca
Srikanth Banagadi <amisrikanth@gmail.com>
2018-09-22 17:53:10 +0530

    changed the port to 80 and added Service Name to OnBoarding

--- OnBoarding/Models/EndUserDto.cs
+++ OnBoarding/Models/EndUserDto.cs
@@ -10,12 +10,12 @@ namespace OnBoarding.Models
         long endUserId;
         string name;
         string profileImageUrl;
-        long organisartionId;
+        long organisationId;
         string organisationEmail;
         public long EndUserId { get => endUserId; set => endUserId = value; }
         public string Name { get => name; set => name = value; }
         public string ProfileImageUrl { get => profileImageUrl; set => profileImageUrl = value; }
-        public long OrganisationId { get => organisartionId; set => organisartionId = value; }
+        public long OrganisationId { get => organisationId; set => organisationId = value; }
         public string OrganisationEmail { get => organisationEmail; set => organisationEmail = value; }
     }
 }
